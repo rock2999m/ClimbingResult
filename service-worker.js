ed3cf3b963436747b462e8a498e345d0ae6cdad8
rock2999m <yuji.fallline2999m@gmail.com>
2026-03-24 13:19:18 +0900

    Update v1.1

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "climbing-session-recorder-v1";
-const APP_SHELL = [
+var CACHE_NAME = "climbing-session-recorder-v1";
+var APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
@@ -7,31 +7,50 @@ const APP_SHELL = [
   "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => Promise.resolve())
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(APP_SHELL);
+    }).catch(function () {
+      return Promise.resolve();
+    })
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", function (event) {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.map(function (key) {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
           return Promise.resolve();
         })
-      )
-    )
+      );
+    })
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", function (event) {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request)
+        .then(function (networkResponse) {
+          var cloned = networkResponse.clone();
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, cloned);
+          });
+          return networkResponse;
+        })
+        .catch(function () {
+          return caches.match("./index.html");
+        });
+    })
+  );
 });
